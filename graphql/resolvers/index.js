be7421929f416dd98f48100cf8fbd12ea4b3d19e
Dan <neveloffd@gmail.com>
2021-04-02 18:00:39 -0400

    user resolvers complete

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+import usersResolvers from "./users.js";
+
+export default {
+  Query: {
+    ...usersResolvers.Query,
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...movieResolvers.Mutation,
+  },
+};
