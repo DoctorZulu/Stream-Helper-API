c0c62cd7e4618f679afccc83d0525d8b65953c1b
Dan <neveloffd@gmail.com>
2021-04-02 17:47:24 -0400

    user resolvers in progress

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,4 +1,4 @@
-import { gql, PubSub } from "apollo-server";
+import { gql } from "apollo-server";
 
 const typeDefs = gql`
   scalar Date
@@ -11,46 +11,47 @@ const typeDefs = gql`
     email: String!
     token: String
     password: String
-    
+
     movies: [Movie]
   }
 
   type Movie {
-      id: ID!
-      title: String
-      original_language: String
-      release_date: String
-      runetime: Int
-      vote_average: Int
-      overview: String
-      image: String
-      genres: [Genre]
-      user: User
-
+    id: ID!
+    title: String
+    original_language: String
+    release_date: String
+    runetime: Int
+    vote_average: Int
+    overview: String
+    image: String
+    genres: [Genre]
+    user: User
   }
   type Genre {
-     id: ID!
-     name: String
-     movies: [Movie] 
+    id: ID!
+    name: String
+    movies: [Movie]
   }
-  
-
 
   # Top level
   type Query {
-      allMovies: [Movie]
-      user: User
-      movie: Movie
-
+    allMovies: [Movie]
+    user: User
+    movie: Movie
   }
   type Mutation {
-  
+    signupUser(signupInput: SignupInput): User!
+    signinUser(email: String!, pass: String!): User!
+    updateUser(username: String, firstname: String, email: String): User
+    addMovie(movieId: ID!, userId: ID!): User!
+    discardMovie(movieId: ID!, userId: ID!): User!
+    saveMovie(movieId: ID!, userId: ID!): User!
   }
   input SignupInput {
-
-  }
-   type Subscription {
- 
+    email: String!
+    firstname: String!
+    lastname: String
+    password: String!
   }
 `;
 
