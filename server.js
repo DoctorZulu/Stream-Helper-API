47f9f5c2f0b485c43daec6b3189726dcc56158d3
victordoyle <victor-doyle@outlook.com>
2021-04-25 00:02:52 -0400

    fix agaain

--- server.js
+++ server.js
@@ -42,13 +42,13 @@ async function startApolloServer() {
   
 
   // Disable until depolyment, ill create a check later ---Sean
-  const corsOptions = {
+/*   const corsOptions = {
     origin: '*',
     credentials: true,
-  };
+  }; */
 
   app.use(express.json());
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(cookieParser());
   app.use(
     cookieSession({
